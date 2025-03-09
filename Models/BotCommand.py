e3d07eeabaa33c7b05c6d81f2452c71fa4c0cbe8
Oskar Eek <oskar.eek99@outlook.com>
2025-03-09 23:13:18 +0100

    BotCommands general handling

---
+++ Models/BotCommand.py
@@ -0,0 +1,43 @@
+from discord import Message
+from typing import Callable, Optional, Awaitable
+
+class BotCommand:
+    command: str
+    operation: Callable[[Optional[Message]], Optional[str]]
+    explanation: str
+
+    def __init__(
+            self,
+            command: str,
+            explanation: str,
+            operation: Callable[[Optional[Message]], Optional[str]],
+            ):
+        self.command = command
+        self.operation = operation
+        self.explanation = explanation
+
+    async def execute(self, message: Message):
+        result = self.operation(message)
+        await self.handle_result(result, message)
+
+    async def handle_result(self, result: Optional[str], message: Message):
+        if result != None:
+            message.channel.send(result)
+
+
+
+class AsyncBotCommand(BotCommand):
+    asyncOperation: Callable[[Optional[Message]], Awaitable[Optional[str]]]
+
+    def __init__(self,
+                command: str,
+                explanation: str,
+                operation: Callable[[Optional[Message]], Awaitable[Optional[str]]]
+                ):
+        self.command = command
+        self.explanation = explanation
+        self.asyncOperation = operation
+
+    async def execute(self, message):
+        result = await self.asyncOperation(message)
+        await self.handle_result(result)
---
+++ botCommands.py
@@ -0,0 +1,24 @@
+from Models.BotCommand import BotCommand, AsyncBotCommand
+from BotFunctions import GrabMemory
+from BotFunctions import Gamble
+from BotFunctions import Random
+from BotFunctions import Help
+from BotFunctions import Lottery
+from BotFunctions import Roulette
+
+botCommands: dict[str, BotCommand] = {
+    ".free-points": AsyncBotCommand(".free-points", "Gives user 500 free points", Gamble.free_points()),
+    ".gamble": AsyncBotCommand(".gamble", "Gamble the number of coins you input with 50/50 chance to win", Gamble.gamble()),
+    ".points": AsyncBotCommand(".points", "Check how many points you have", Gamble.points()),
+    ".challange": AsyncBotCommand(".challange", "", Gamble.challange()),
+    ".respond": AsyncBotCommand(".respond", "", Gamble.respond_challange()),
+    ".leaderboard": AsyncBotCommand(".leaderboard", "", Gamble.leaderboard()),
+    ".grab-memory": AsyncBotCommand(".grab-memory", "", GrabMemory.grab_memory()),
+    ".random": AsyncBotCommand(".random", "", Random.randomizer()),
+    ".help": AsyncBotCommand(".help", "", Help.help()),
+    ".lottery": AsyncBotCommand(".lottery", "", Lottery.start_lottery()),
+    ".enter": AsyncBotCommand(".enter", "", Lottery.add_lottery_points()),
+    ".end-lottery": AsyncBotCommand(".end-lottery", "", Lottery.end_lottery()),
+    ".roulette": AsyncBotCommand(".roulette", "", Roulette.roulette()),
+    ".end-roulette": AsyncBotCommand(".end-roulette", "", Roulette.end_roulette()),
+}

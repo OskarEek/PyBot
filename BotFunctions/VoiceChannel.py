7513b33f2fcd9cf5fd16dacb67802f4a180f205e
Oskar Eek <oskar.eek99@outlook.com>
2025-03-30 00:49:30 +0100

    Disconnect and validation for playing yt links

--- BotFunctions/VoiceChannel.py
+++ BotFunctions/VoiceChannel.py
@@ -12,13 +12,17 @@ async def play(message: Message):
     inputs = UserInputService.get_user_input(message.content, inputs)
     url = inputs[0].get_value()
 
-    if message.author.voice:
-        channel = message.author.voice.channel
-        client = await channel.connect()
+    if not "youtube.com/" in url:
+        return "You need to enter a youtube link"
+    if not message.author.voice:
+        return "You need to be in a voice channel"
 
-        player = await YTDLSource.from_url(url=url, stream=True)
-        print(player)
-        client.play(player)
+    channel = message.author.voice.channel
+    client = await channel.connect()
+    player = await YTDLSource.from_url(url=url, stream=True)
+    print(player)
+    await client.play(player)
+    await client.disconnect()
         
         
         
